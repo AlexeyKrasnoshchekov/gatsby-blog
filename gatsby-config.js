b55c628b05a664a96a28f69b8c0a383c5c92481f
Алексей <alexburnitdown@gmail.com>
2021-09-27 16:02:59 +0300

    styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,21 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
+    {
       resolve: "gatsby-plugin-mdx",
       options: {
         extentions: [
---
+++ src/themes/theme.js
@@ -0,0 +1,27 @@
+export default {
+    fonts: {
+        main: "",
+        code: "",
+    },
+    colors: {
+        main1: ""
+    },
+    breakpoints: {
+        mobile: "only screen and (max-widthL 50rem)",
+        tablet: "only screen and (max-widthL 65rem)",
+    },
+    spacings: {
+        xxSmall: ".25rem",
+        xSmall: ".5rem",
+        small: "1rem",
+        medium: "2rem",
+        large: "3rem"
+    },
+    animations: {
+        button: "box-shadow 0.3s ease",
+        link: "color 0.2s ease"
+    },
+    shadows: {
+        shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)"
+    }
+}
